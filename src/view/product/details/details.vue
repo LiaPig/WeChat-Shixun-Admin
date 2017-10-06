92910641b1b7ce9e5345dad1f216d0c0a0e516c6
LeeWaiHo <791783391@qq.com>
2017-10-07 02:05:05 +0800

    修改查看详情页面

--- src/view/product/details/details.vue
+++ src/view/product/details/details.vue
@@ -1,105 +1,96 @@
 <template>
-    <div class="detail_panel">
-      <!--表单开始-->
-      <el-form label-position="right" label-width="80px" :model="product_data">
-        <!--名称开始-->
-        <el-form-item label="名称" class="item">
-          <el-input v-model="product_data.name"></el-input>
-        </el-form-item>
-        <!--名称结束-->
+  <div class="detail_panel">
+    <!--表单开始-->
+    <el-form label-position="right" label-width="80px" :model="product_data">
+      <!--id开始-->
+      <el-form-item label="id" class="item">
+        <el-input :disabled="true" :value="product_data.id"></el-input>
+      </el-form-item>
+      <!--id结束-->
 
-        <!--id开始-->
-        <el-form-item label="id" class="item">
-          <el-input v-model="product_data.id"></el-input>
-        </el-form-item>
-        <!--id结束-->
+      <!--名称开始-->
+      <el-form-item label="名称" class="item">
+        <el-input v-model="product_data.name" :disabled="true"></el-input>
+      </el-form-item>
+      <!--名称结束-->
 
-        <!--状态开始-->
-        <el-form-item label="状态" class="item">
-          <el-input v-model="product_data.status"></el-input>
-        </el-form-item>
-        <!--状态结束-->
+      <!--状态开始-->
+      <el-form-item label="状态" class="item">
+        <el-input v-model="product_data.status" :disabled="true"></el-input>
+      </el-form-item>
+      <!--状态结束-->
 
-        <!--底价开始-->
-        <el-form-item label="底价" class="item">
-          <el-input v-model="product_data.basePrice"></el-input>
-        </el-form-item>
-        <!--底价结束-->
+      <!--底价开始-->
+      <el-form-item label="底价" class="item">
+        <el-input v-model="product_data.basePrice" :disabled="true"></el-input>
+      </el-form-item>
+      <!--底价结束-->
 
-        <!--详细描述开始-->
-        <el-form-item label="详细描述" class="item">
-          <el-input v-model="product_data.description"></el-input>
-        </el-form-item>
-        <!--详细描述结束-->
+      <!--详细描述开始-->
+      <el-form-item label="详细描述" class="item">
+        <el-input v-model="product_data.description" :disabled="true"></el-input>
+      </el-form-item>
+      <!--详细描述结束-->
 
-        <!--属性标签开始-->
-        <div class="productTags">
-          <div class="title">属性标签</div>
-          <div class="content" v-if="product_data.productTags.length > 0">
-            <el-checkbox-group v-model="product_data.productTags" fill="#324057" text-color="#a4aebd">
-              <el-checkbox-button v-for="(item, index) in product_data.productTags" :label="'标签'+ index" :key="index" v-if="item.type=='product'">{{item.name}}</el-checkbox-button>
-            </el-checkbox-group>
-          </div>
-          <div class="content" v-if="product_data.productTags.length == 0">
-            此商品无属性标签
-          </div>
+      <!--标签开始-->
+      <el-form-item label="商品标签" prop="productTags" class="item">
+        <el-tag
+          v-if="product_data.productTags.length > 0"
+          v-for="tag in product_data.productTags"
+          :key="tag.id"
+          type="success"
+          :closable="false"
+        >
+          {{tag.name}}
+        </el-tag>
+        <div class="content" v-if="product_data.productTags.length == 0">
+          此商品无属性标签
         </div>
-        <!--属性标签结束-->
+      </el-form-item>
+      <!--标签结束-->
 
-        <!--分类标签开始-->
-        <div class="productTags">
-          <div class="title">分类标签</div>
-          <div class="content">
-            <el-checkbox-group v-model="product_data.productTags" fill="#324057" text-color="#a4aebd">
-              <el-checkbox-button v-for="(item, index) in product_data.productTags" :label="'标签'+ index" :key="index" v-if="item.type=='productCategory'">{{item.name}}</el-checkbox-button>
-            </el-checkbox-group>
-          </div>
-          <div class="content" v-if="product_data.productTags.length == 0">
-            此商品无分类标签
-          </div>
+      <!--图片开始-->
+      <el-form-item label="图片" class="item" v-if="product_data.productImages == 0">
+        <div class="content" v-if="product_data.productImages.length == 0">
+          此商品无图片
         </div>
-        <!--分类标签结束-->
-
-        <!--图片开始-->
-        <div class="productImages">
-          <div class="title">图片</div>
-          <div class="content" v-for="(item, index) in product_data.productImages">
-            <el-popover
-              title="url为:"
-              placement="top-start"
-              width="200"
-              trigger="hover">
-              <div style="display: inline-block;width: 200px;word-break:break-all;">
-                <p>{{item.url}}</p>
-                <img :src="item.url" style="width: 150px;height: 150px;"/>
-              </div>
-              <el-button  slot="reference">查看图片{{index+1}}</el-button>
-            </el-popover>
+      </el-form-item>
+      <el-form-item :label="'图片' + (index + 1)" class="item" v-for="(item, index) in product_data.productImages"
+                    v-if="product_data.productImages.length > 0">
+        <el-popover
+          placement="top-start"
+          width="200"
+          trigger="hover">
+          <div style="display: inline-block;width: 200px;word-break:break-all;">
+            <img :src="item.url" style="width: auto; height: 150px;"/>
           </div>
-        </div>
-        <!--图片结束-->
+          <el-input :disabled="true" v-model="item.url" slot="reference"></el-input>
+        </el-popover>
+      </el-form-item>
+      <!--图片结束-->
 
-      </el-form>
-      <!--表单结束-->
+    </el-form>
+    <!--表单结束-->
 
-      <!--按钮组开始-->
-      <el-button-group class="group">
-        <el-button @click="$router.back()" type="primary" icon="arrow-left">返回上层</el-button>
-        <router-link :to="{
+    <!--按钮组开始-->
+    <el-button-group class="group">
+      <el-button @click="$router.back()" type="primary" icon="arrow-left">返回上层</el-button>
+      <router-link :to="{
                                 name: 'productModify',
                                 params: {id: this.product_data.id}
                               }">
-          <el-button type="primary">
-            修改信息<i class="el-icon-arrow-right el-icon--right"></i>
-          </el-button>
-        </router-link>
-      </el-button-group>
-      <!--按钮组结束-->
-    </div>
+        <el-button type="primary">
+        修改信息<i class="el-icon-arrow-right el-icon--right"></i>
+        </el-button>
+      </router-link>
+    </el-button-group>
+    <!--按钮组结束-->
+  </div>
 </template>
 
 <script>
   import ElButtonGroup from '../../../../node_modules/element-ui/packages/button/src/button-group'
+
   export default {
     components: {ElButtonGroup},
     created () {
@@ -110,8 +101,7 @@
     data () {
       return {
         route_id: this.$route.params.id,
-        product_data: {
-        }
+        product_data: {}
       }
     },
     methods: {
@@ -119,7 +109,7 @@
       get_product_data: function (id) {
         const that = this
         that.load_data = true
-        that.$http.get('/api/products/' + id )
+        that.$http.get('/api/products/' + id)
           .then((response) => {
             that.product_data = response.body.data
             that.load_data = false
@@ -141,6 +131,7 @@
     width: 40%;
     padding-right: 40%;
   }
+
   .productTags {
     float: left;
     margin-left: 18.5%;
@@ -148,6 +139,7 @@
     padding-right: 40%;
     padding-bottom: 20px;
   }
+
   .productTags .title {
     float: left;
     display: inline-block;
@@ -158,11 +150,13 @@
     margin-top: 0;
     padding-left: 30px;
   }
+
   .group {
     display: inline-block;
     width: 67%;
     padding-left: 33%;
   }
+
   .productImages {
     float: left;
     margin-left: 18.5%;
@@ -170,6 +164,7 @@
     padding-right: 40%;
     padding-bottom: 20px;
   }
+
   .productImages .title {
     float: left;
     display: inline-block;
@@ -182,6 +177,7 @@
     text-align: right;
     margin-right: 10px;
   }
+
   .productImages button {
     display: inline-block;
     float: left;
