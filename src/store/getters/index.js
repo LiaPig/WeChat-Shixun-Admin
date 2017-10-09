634e07fe979dae0792ccc874c8ee9be980e14398
ailgiP <41807528@qq.com>
2017-10-09 17:51:51 +0800

    登录还是做不出5

--- src/store/getters/index.js
+++ src/store/getters/index.js
@@ -2,16 +2,6 @@
  * Created by MINT on 2017/9/29.
  */
 // getters里放获取states里的数据的方法
-import * as type from './type.js'
 
 export default {
-  [type.GET_TOKEN_INFO]: state => {
-    return state.Token
-  },
-  [type.GET_USER_INFO]: state => {
-    return state.User
-  },
-  test: state => {
-    return state.a
-  }
 }
--- src/store/mutations/index.js
+++ src/store/mutations/index.js
@@ -1,28 +1,6 @@
 /**
  * Created by MINT on 2017/9/29.
  */
-import * as type from './type.js'
-import {sessionStorage} from 'Common/storage'
-// mutations里放修改state里的数据的方法
+
 export default {
-  // 设置TOKEN信息
-  SET_TOKEN_INFO (state, tokeninfo) {
-    console.log('hi')
-    state.Token = tokeninfo || {}
-    if (tokeninfo === null) {
-      sessionStorage.remove('token_info')
-    } else {
-      sessionStorage.set('token_info', tokeninfo)
-      console.log(tokeninfo)
-    }
-  },
-  // 设置USER信息
-  [type.SET_USER_INFO] (state, userinfo) {
-    state.User = userinfo || {}
-    if (userinfo === null) {
-      sessionStorage.remove('user_info')
-    } else {
-      sessionStorage.set('user_info', userinfo)
-    }
-  }
 }
