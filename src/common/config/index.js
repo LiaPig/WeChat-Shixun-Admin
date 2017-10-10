f587659bd43140035b745c787125f9b12ad1572d
LeeWaiHo <791783391@qq.com>
2017-10-10 16:12:27 +0800

    修改storage前缀

--- src/common/config/index.js
+++ src/common/config/index.js
@@ -2,7 +2,7 @@
  * Created by MINT on 2017/9/29.
  */
 // 本地存储的前缀
-export const storagePrefix = 'admin_storage_'
+export const storagePrefix = 'zhuzai_storage_'
 // base64加密前缀
 export const base64Prefix = 'admin_base64_'
 // 接入服务器接口地址根目录
